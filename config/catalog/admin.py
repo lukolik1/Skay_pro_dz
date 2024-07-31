e4425ab4d14856edcd3d30c1a75393e752b74b54
lukolik1 <111924837+lukolik1@users.noreply.github.com>
2024-07-31 17:11:23 +0300

    New models and admin panel and shell django ORM

--- config/catalog/admin.py
+++ config/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description')
